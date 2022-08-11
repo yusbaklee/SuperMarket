2f175787d751d960993715f06e010dff73d80195
yusbaklee <67943334+yusbaklee@users.noreply.github.com>
2022-08-11 10:09:33 +0100

    Add project files.

---
+++ bank/Program.cs
@@ -0,0 +1,24 @@
+﻿using SOLID_PRINCIPLES.OCP;
+namespace SOLID_PRINCIPLES
+{
+    /// <summary>
+    /// this is the main method of the invoice
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Invoice FInvoice = new FinalInvoice();
+            Invoice PInvoice = new ProposedInvoice();
+            Invoice RInvoice = new RecurringInvoice();
+            double FInvoiceAmount = FInvoice.GetInvoiceDiscount(1000);
+            double PInvoiceAmount = PInvoice.GetInvoiceDiscount(1000);
+            double RInvoiceAmount = RInvoice.GetInvoiceDiscount(1000);
+            Console.WriteLine("enter your Amount");
+            Console.ReadLine();
+            Console.WriteLine("This is the final invoice:  "+FInvoiceAmount);
+            Console.WriteLine("This is the Proposed invoice: "+ PInvoiceAmount);
+            Console.WriteLine("This is the RecurringInvoice: "+RInvoiceAmount);
+        }
+    }
+}
---
+++ bank/Promo.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INVOICE
+{
+    /// <summary>
+    /// This is the abstract class
+    /// </summary>
+    public abstract class INVOICE
+    {
+        public double FinalInvoice { get; set; }
+        public double TotalInvoice { get; set; }
+
+
+    }
+}
+
+
+internal class INVOICE2 { }
+
+namespace SOLID_PRINCIPLES.OCP
+{
+    /// <summary>
+    /// InvoiceType Discount
+    /// </summary>
+    public class Invoice
+    {
+        public virtual double GetInvoiceDiscount(double amount)
+        {
+            return amount - 10;
+        }
+    }
+
+    /// <summary>
+    /// This is the FinalInvoice Class with the dicount method
+    /// </summary>
+    public class FinalInvoice : Invoice
+    {
+        public override double GetInvoiceDiscount(double amount)
+        {
+            return  base.GetInvoiceDiscount(amount) - 50;
+        }
+    }
+    /// <summary>
+    /// This is the ProposedInvoice Class with the discount method
+    /// </summary>
+    public class ProposedInvoice : Invoice
+    {
+        public override double GetInvoiceDiscount(double amount)
+        {
+            try
+            {
+
+                return base.GetInvoiceDiscount(amount) - 40;
+            }
+            catch (Exception)
+            {
+
+                return 0;
+            }
+        }
+    }
+    /// <summary>
+    /// This is the RecurringInvoice Class with the discount method
+    /// </summary>
+    public class RecurringInvoice : Invoice
+    {
+        public override double GetInvoiceDiscount(double amount)
+        {
+            return base.GetInvoiceDiscount(amount) - 30;
+        }
+    }
+}
